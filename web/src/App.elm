5a6efb80f412cac8d40320f26b808d21f658181b
Carmine Moleti <carmine@moleti.it>
2017-03-13 17:53:01 +0100

    Branching out to evaluate standard non WebComponents MDL

--- web/src/App.elm
+++ web/src/App.elm
@@ -1,7 +1,9 @@
 module App exposing (..)
 
+import Debug
 import Html exposing (Html, text, div, img, node)
 import Html.Attributes exposing (attribute, class, src)
+import Html.Events exposing (onClick)
 import Polymer.App as PA
 import Polymer.Attributes exposing (icon, label)
 import Polymer.Paper as Paper
@@ -20,11 +22,16 @@ init path =
 
 type Msg
     = NoOp
+    | FirstRowSelect
+    | SecondRowSelect
+    | GenericSelect String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case (Debug.log "Update" (toString msg)) of
+        _ ->
+            ( model, Cmd.none )
 
 
 view : Model -> Html Msg
@@ -39,6 +46,7 @@ view model =
                     , class "secondary"
                     ]
                     [ text "Aggiungi preset" ]
+                , table
                 ]
             ]
         ]
@@ -67,6 +75,57 @@ header model =
                 [ text "Guitar Router" ]
             , div []
                 [ node "iron-icon" [ class "logo", src model.logo ] [] ]
+            ]
+        ]
+
+
+table : Html Msg
+table =
+    div []
+        [ Paper.iconButton [ icon "favorite", onClick (GenericSelect "Hi") ] []
+        , node "vaadin-grid"
+            []
+            [ Html.table
+                []
+                [ Html.colgroup
+                    []
+                    [ Html.col [ attribute "flex" "1" ] []
+                    , Html.col [ attribute "flex" "1" ] []
+                    , Html.col [ attribute "flex" "1" ] []
+                    , Html.col [ attribute "flex" "1" ] []
+                    , Html.col [ attribute "flex" "1" ] []
+                    ]
+                , Html.thead
+                    []
+                    [ Html.tr
+                        []
+                        [ Html.th [] [ text "Id" ]
+                        , Html.th [] [ text "Nome" ]
+                        , Html.th [] [ text "Valore" ]
+                        , Html.th [] [ text "Enabled" ]
+                        , Html.th [] [ text "Azioni" ]
+                        ]
+                    ]
+                , Html.tbody
+                    []
+                    [ Html.tr
+                        []
+                        [ Html.td [] [ text "0" ]
+                        , Html.td [] [ text "Carmine" ]
+                        , Html.td [] [ text "74" ]
+                        , Html.td [] [ node "paper-checkbox" [ attribute "checked" "" ] [] ]
+                        , Html.td [] [ Paper.iconButton [ attribute "icon" "icons:build", onClick FirstRowSelect ] [] ]
+                        ]
+                    , Html.tr
+                        []
+                        [ Html.td [] [ text "1" ]
+                        , Html.td [] [ text "Francesco" ]
+                        , Html.td [] [ text "77" ]
+                        , Html.td [] [ node "paper-checkbox" [ attribute "checked" "" ] [] ]
+                        , Html.td [] [ Paper.iconButton [ attribute "icon" "icons:build", onClick SecondRowSelect ] [] ]
+                        ]
+                    ]
+                ]
             ]
         ]
 
