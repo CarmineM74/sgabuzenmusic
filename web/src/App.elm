2b5e74ba389210a14575296f45a20c09d3fad45b
Carmine Moleti <carmine@moleti.it>
2017-03-15 08:49:13 +0100

    Back to initial state. Application launches. Stylesheets and fonts get loaded

--- web/src/App.elm
+++ web/src/App.elm
@@ -1,175 +1,68 @@
 module App exposing (..)
 
 import Debug
-import Html exposing (Html, text, div, img, node)
+import Html exposing (Html, text, div, h1, img, node, p)
 import Html.Attributes exposing (attribute, class, src)
 import Html.Events exposing (onClick, on)
 import Json.Decode as JD
-import Polymer.App as PA
-import Polymer.Attributes exposing (icon, label)
-import Polymer.Paper as Paper
+import Material
+import Material.Layout as Layout
 
 
 type alias Model =
     { message : String
     , logo : String
+    , mdl : Material.Model
     }
 
 
 init : String -> ( Model, Cmd Msg )
 init path =
-    ( { message = "Guitar router is loading ...", logo = path }, Cmd.none )
+    ( { message = "Guitar router is loading ..."
+      , logo = path
+      , mdl = Material.model
+      }
+    , Layout.sub0 Mdl
+    )
 
 
 type Msg
     = NoOp
-    | GenericSelect String
-    | SelectedItemChanged
+    | Mdl (Material.Msg Msg)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case (Debug.log "Update" (toString msg)) of
+    case msg of
+        Mdl msg_ ->
+            Material.update Mdl msg_ model
+
         _ ->
             ( model, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
-    PA.drawerLayout []
-        [ drawer
-        , PA.headerLayout []
-            [ header model
-            , tableCard model
-            ]
-        ]
-
-
-drawer : Html Msg
-drawer =
-    PA.drawer []
-        [ div [ class "drawer-contents" ] [] ]
+    Layout.render Mdl
+        model.mdl
+        [ Layout.fixedHeader ]
+        { header = [ header model ]
+        , drawer = []
+        , tabs = ( [], [] )
+        , main = [ body model ]
+        }
 
 
 header : Model -> Html Msg
 header model =
-    PA.header
-        [ attribute "fixed" ""
-        , attribute "effects" "waterfall"
-        ]
-        [ PA.toolbar []
-            [ Paper.iconButton
-                [ icon "menu"
-                  --                , attribute "drawer-toggle" ""
-                ]
-                []
-            , div
-                [ attribute "main-title" "" ]
-                [ text "Guitar Router" ]
-            , div []
-                [ node "iron-icon" [ class "logo", src model.logo ] [] ]
-            ]
-        ]
+    text "I'm the Head ... er"
 
 
-tableCard : Model -> Html Msg
-tableCard model =
-    Paper.card
-        [ attribute "heading" "Presets"
-        , attribute "elevation" "3"
-        ]
-        [ div
-            [ class "card-content" ]
-            [ table model ]
-        , div
-            [ class "card-actions" ]
-            [ Paper.button [ class "primary" ] [ text "Aggiungi" ]
-            , Paper.button [ class "secondary" ] [ text "Aggiorna" ]
-            , Paper.button [ class "secondary", attribute "disabled" "" ] [ text "Modifica" ]
-            , Paper.button [ class "secondary", attribute "disabled" "" ] [ text "Elimina" ]
-            ]
-        ]
-
-
-decodeSelectedItemChanged : JD.Decoder Msg
-decodeSelectedItemChanged =
-    JD.succeed SelectedItemChanged
-
-
-table : Model -> Html Msg
-table model =
-    div []
-        [ node "vaadin-grid"
-            [ attribute "visible-rows" "09"
-            , on "selected-items-changed" decodeSelectedItemChanged
-            ]
-            [ Html.table
-                []
-                [ Html.colgroup
-                    []
-                    [ Html.col [ attribute "flex" "1" ] []
-                    , Html.col [ attribute "flex" "1" ] []
-                    , Html.col [ attribute "flex" "1" ] []
-                    , Html.col [ attribute "flex" "1" ] []
-                    ]
-                , Html.thead
-                    []
-                    [ Html.tr
-                        []
-                        [ Html.th [] [ text "Id" ]
-                        , Html.th [] [ text "Nome" ]
-                        , Html.th [] [ text "Valore" ]
-                        , Html.th [] [ text "Enabled" ]
-                        ]
-                    ]
-                , Html.tbody
-                    []
-                    (tableBody model)
-                ]
-            ]
-        ]
-
-
-tableBody : Model -> List (Html Msg)
-tableBody model =
-    [ Html.tr
-        []
-        [ Html.td [] [ text "0" ]
-        , Html.td [] [ text "Carmine" ]
-        , Html.td [] [ text "74" ]
-        , Html.td [] [ node "paper-checkbox" [ attribute "checked" "" ] [] ]
-        ]
-    , Html.tr
-        []
-        [ Html.td [] [ text "1" ]
-        , Html.td [] [ text "Francesco" ]
-        , Html.td [] [ text "77" ]
-        , Html.td [] [ node "paper-checkbox" [ attribute "checked" "" ] [] ]
-        ]
-    , Html.tr
-        []
-        [ Html.td [] [ text "2" ]
-        , Html.td [] [ text "Anna" ]
-        , Html.td [] [ text "80" ]
-        , Html.td [] [ node "paper-checkbox" [ attribute "checked" "" ] [] ]
-        ]
-    , Html.tr
-        []
-        [ Html.td [] [ text "3" ]
-        , Html.td [] [ text "Enrico" ]
-        , Html.td [] [ text "09" ]
-        , Html.td [] [ node "paper-checkbox" [ attribute "checked" "" ] [] ]
-        ]
-    , Html.tr
-        []
-        [ Html.td [] [ text "4" ]
-        , Html.td [] [ text "Assunta" ]
-        , Html.td [] [ text "17" ]
-        , Html.td [] [ node "paper-checkbox" [ attribute "checked" "" ] [] ]
-        ]
-    ]
+body : Model -> Html Msg
+body model =
+    div [] [ p [] [ text "Something to see now." ] ]
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    Layout.subs Mdl model.mdl
--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,4 @@
-window.addEventListener('WebComponentsReady', () => {
-  var logoPath = require('../static/sgabuzen_music_logo.jpg');
-  let Elm = require('./Main.elm')
-  let root = document.getElementById('root')
-  let app = Elm.Main.embed(root, logoPath)
-})
+var logoPath = require('../static/sgabuzen_music_logo.jpg');
+let Elm = require('./Main.elm')
+let root = document.getElementById('root')
+let app = Elm.Main.embed(root, logoPath)
