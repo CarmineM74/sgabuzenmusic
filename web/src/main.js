c1f63aec9b664eec50a653d0d02abd260aafd44e
Carmine Moleti <carmine@moleti.it>
2017-09-20 14:20:48 +0200

    Added vue-router and vuex.
    Added just one route to show the default page.

--- web/src/main.js
+++ web/src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+import { routes } from './routes'
+
 import App from './App.vue'
+
+Vue.use(Vuex)
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: routes,
+  mode: 'history'
+
+})
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router: router
 })
